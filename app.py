97c0afaec6d16f6f69abfd86b74dce0035754661
xizho10 <342091024@qq.com>
2016-06-18 18:19:04 -0700

    first commit3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis', port=53810)
 
 @app.route('/')
 def hello():
