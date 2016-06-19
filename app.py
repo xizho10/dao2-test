53d1420613495ac46bd9b89550ab926195615370
xizho10 <342091024@qq.com>
2016-06-18 19:11:04 -0700

    first commit4

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=53810)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    return 'Hello World! I have been seen %s times.===' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="10.10.189.180", debug=True)
